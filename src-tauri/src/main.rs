963bc5f4f041795bbb0e896cd7ed3c6e58f049d2
Daniel Scapulatempo <danielscapulatempo@gmail.com>
2023-06-02 04:24:33 -0300

    add updater

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod commands;
 
@@ -8,12 +8,15 @@ use commands::timecard_get_by_id;
 use commands::event_get_by_id;
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            card_get_by_id,
-            timecard_get_by_id,
-            event_get_by_id
-        ])
+    let app = tauri::Builder::default()
+                .invoke_handler(tauri::generate_handler![
+                    card_get_by_id,
+                    timecard_get_by_id,
+                    event_get_by_id
+                ]);
+
+    app
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -14,7 +14,7 @@ async function greet() {
 <template>
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
+    <button type="button" @click="greet()">Consultar cartão</button>
   </div>
 
   <p>{{ greetMsg }}</p>
